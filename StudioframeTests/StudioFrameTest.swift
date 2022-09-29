9a4d79d4ad0d01c35aa5a1b0af553629d7778ca8
Destrarion <Destarion@gmail.com>
2022-09-29 09:31:16 +0200

    added test in random order

--- StudioframeTests/StudioFrameTest.swift
+++ StudioframeTests/StudioFrameTest.swift
@@ -12,13 +12,18 @@ import XCTest
 final class StudioFrameTest: XCTestCase {
     
     
+    var usdzLibraryService: UsdzLibraryService!
+    
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        usdzLibraryService = UsdzLibraryService()
+        usdzLibraryService.deleteAllFavorite()
     }
+    
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        usdzLibraryService.deleteAllFavorite()
     }
 
     func testExample() throws {
@@ -37,28 +42,21 @@ final class StudioFrameTest: XCTestCase {
     }
     
 
-    
     // MARK: - Favorite test
     
     func test_givenUsdzLibraryService_whenUsdzLibraryServiceInit_thenAddAirForceInFavorite() {
-        let usdzLibraryServiceTest = UsdzLibraryService()
-        usdzLibraryServiceTest.deleteAllFavorite()
-        usdzLibraryServiceTest.addFavorite(usdzObject: UsdzObject(title: "AirForce", objectUrlString: "should be done at init", thumbnailImageUrlString: "but Xcode randomize test when i unchecked randomise anyway"))
-        let favorite = usdzLibraryServiceTest.getFavoriteObjects()
+        usdzLibraryService.addFavorite(usdzObject: UsdzObject(title: "AirForce", objectUrlString: "should be done at init", thumbnailImageUrlString: "but Xcode randomize test when i unchecked randomise anyway"))
+        let favorite = usdzLibraryService.getFavoriteObjects()
         XCTAssertEqual(favorite.first!.title, "AirForce")
     }
     
     func test_givenNoFavorite_whenAddFavorite_thenFavoriteAdded() {
-        let usdzLibraryServiceTest = UsdzLibraryService()
-        usdzLibraryServiceTest.deleteAllFavorite()
         let usdzToAdd = UsdzObject(title: "UsdzToAdd", objectUrlString: "To add", thumbnailImageUrlString: "none")
-        usdzLibraryServiceTest.addFavorite(usdzObject: usdzToAdd)
-        XCTAssertEqual(usdzLibraryServiceTest.getFavoriteObjects().first?.title, usdzToAdd.title)
+        usdzLibraryService.addFavorite(usdzObject: usdzToAdd)
+        XCTAssertEqual(usdzLibraryService.getFavoriteObjects().first?.title, usdzToAdd.title)
     }
     
     func test_givenNoFavoriteUsdz_whenAddFavoriteUsdz_thenUsdzFavoriteAdded() {
-        let usdzLibraryService = UsdzLibraryService()
-        usdzLibraryService.deleteAllFavorite()
         let usdz = UsdzObject(title: "usdz", objectUrlString: "1.2.3", thumbnailImageUrlString: "none")
         usdzLibraryService.addFavorite(usdzObject: usdz)
         let favorite = usdzLibraryService.getFavoriteObjects()
@@ -69,8 +67,6 @@ final class StudioFrameTest: XCTestCase {
     }
     
     func test_givenAirForceFavorite_whenRemoveFavoriteAirForce_thenNoFavoriteRemain() {
-        let usdzLibraryService = UsdzLibraryService()
-        usdzLibraryService.deleteAllFavorite()
         let airForce = UsdzObject(title: "AirForce", objectUrlString: "3.2.1", thumbnailImageUrlString: "none")
         usdzLibraryService.addFavorite(usdzObject: airForce)
         var favoriteUsdz = usdzLibraryService.getFavoriteObjects()
@@ -81,17 +77,13 @@ final class StudioFrameTest: XCTestCase {
     }
     
     
-    func test_givenUsdzFavorited_whenCheckFavorite_thenReturnTrue(){
-        let usdzLibraryService = UsdzLibraryService()
-        usdzLibraryService.deleteAllFavorite()
+    func test_givenUsdzFavorited_whenCheckFavorite_thenReturnTrue() {
         let usdzCheckFavorite = UsdzObject(title: "usdzCheckFavoriteTrue", objectUrlString: "Favorite should be true", thumbnailImageUrlString: "none")
         usdzLibraryService.addFavorite(usdzObject: usdzCheckFavorite)
         XCTAssertTrue(usdzLibraryService.checkFavorite(usdzobject: usdzCheckFavorite))
     }
     
-    func test_givenZeroUsdzFavorited_whenCheckFavorite_thenReturnFalse(){
-        let usdzLibraryService = UsdzLibraryService()
-        usdzLibraryService.deleteAllFavorite()
+    func test_givenZeroUsdzFavorited_whenCheckFavorite_thenReturnFalse() {
         let usdzCheckFavorite = UsdzObject(title: "usdzCheckFavoriteFalse", objectUrlString: "Favorite should be false", thumbnailImageUrlString: "none")
         XCTAssertFalse(usdzLibraryService.checkFavorite(usdzobject: usdzCheckFavorite))
     }
@@ -99,15 +91,13 @@ final class StudioFrameTest: XCTestCase {
     
     //MARK: - USDZ File test
     
-    func test_givenUsdzLibrary_whenFetchAllLocalUsdz_thenReturnLocalUsdz(){
-        let usdzLibraryService = UsdzLibraryService()
+    func test_givenUsdzLibrary_whenFetchAllLocalUsdz_thenReturnLocalUsdz() {
         guard let localUsdz = try? usdzLibraryService.getLocalObjects() else { return XCTFail("error getting local object")}
         print("🚧", localUsdz)
         XCTAssertEqual(localUsdz.count, 1)
     }
     
     func test_givenAirForceUsdzFile_whenCheckingIfAlreadyDownloaded_thenReturnTrue() {
-        let usdzLibraryService = UsdzLibraryService()
         guard let localUsdz = try? usdzLibraryService.getLocalObjects() else { return XCTFail("error getting local object")}
         XCTAssertTrue(usdzLibraryService.getIsDownload(usdzObject: localUsdz.first!))
         
